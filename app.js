3ffbb2ce8f3c4e659f4332c13287bd583abd4ae3
JUNYOUNG KIM <46435463+KimJunYoung1@users.noreply.github.com>
2019-12-10 14:48:38 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './src/redux';
+// npm run tsc후 './build/navigator/stack-navi.tsx'로 변경해주세요!
 import Appcontainer from './src/navigator/stack-navi.tsx';
 
 if (__DEV__) {
